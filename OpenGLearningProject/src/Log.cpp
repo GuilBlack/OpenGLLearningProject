b156681ac13f3e8b731133261b811daa5b90706d
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2022-07-29 17:31:13 +0400

    Simple logger added.

---
+++ OpenGLearningProject/src/Log.cpp
@@ -0,0 +1,11 @@
+#include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+std::shared_ptr<spdlog::logger> Log::s_EngineLogger;
+
+void Log::Init()
+{
+	spdlog::set_pattern("%^[%T] [thread %t] %n: %v%$");
+	s_EngineLogger = spdlog::stdout_color_mt("EngineLog");
+	s_EngineLogger->set_level(spdlog::level::trace);
+}
---
+++ OpenGLearningProject/src/Log.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+
+class Log
+{
+public:
+	static void Init();
+	inline static std::shared_ptr<spdlog::logger> GetEngineLogger() { return s_EngineLogger; }
+
+private:
+	static std::shared_ptr<spdlog::logger> s_EngineLogger;
+};
+
+#ifdef DEBUG
+
+#define ENGINE_ERROR(...) Log::GetEngineLogger()->error(__VA_ARGS__)
+#define ENGINE_WARN(...)  Log::GetEngineLogger()->warn(__VA_ARGS__)
+#define ENGINE_INFO(...)  Log::GetEngineLogger()->info(__VA_ARGS__)
+#define ENGINE_TRACE(...) Log::GetEngineLogger()->trace(__VA_ARGS__)
+
+#else
+
+#define ENGINE_ERROR(...)
+#define ENGINE_WARN(...)
+#define ENGINE_INFO(...)
+#define ENGINE_TRACE(...)
+
+#endif // DEBUG
+
